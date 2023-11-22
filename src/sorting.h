719317695725c42bface4f70ef1dc4b5642df884
PereB <pere.balsach@gmail.com>
2023-11-22 19:08:22 +0100

    Added sorting files

---
+++ src/sorting.h
@@ -0,0 +1,8 @@
+#ifndef SORTING_H
+#define SORTING_H
+
+void quick_sort(int arr[], int low, int high);
+int partition(int arr[], int low, int high);
+void swap(int* a, int* b);
+
+#endif //SORTING_H
